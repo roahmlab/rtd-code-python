5adad13507e99bd4442f64b7f02688e5e4eb94ea
Orbital-Web <36625832+Orbital-Web@users.noreply.github.com>
2023-10-30 21:40:06 -0400

    fix: added nptyping and pyvista to setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         install_requires=[
                 'mergedeep>=1.3',
                 'numpy>=1.24',
-                'nptyping>=2.5.0'
+                'nptyping>=2.5.0',
                 'matplotlib>=3.7',
                 'trimesh>=3.21',
                 'python-fcl>=0.7',
